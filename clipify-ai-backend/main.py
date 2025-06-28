d24005b364a33c5364b24905f96b3002a3c816b6
Garvit Rai <raigarvit9@gmail.com>
2025-06-28 12:31:39 +0530

    Added WhisperX for trascription and Gemini API for viral Moment detection in the backend

--- clipify-ai-backend/main.py
+++ clipify-ai-backend/main.py
@@ -1,7 +1,25 @@
-from fastapi import Depends
+import glob
+import json
+import pathlib
+import pickle
+import shutil
+import subprocess
+import time
+import uuid
+import boto3
+import cv2
+from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+import ffmpegcv
 import modal
+import numpy as np
 from pydantic import BaseModel
+import os
+from google import genai
+
+import pysubs2
+from tqdm import tqdm
+import whisperx
 
 class ProcessVideoRequest(BaseModel):
     s3_key:str
@@ -29,13 +47,122 @@ auth_scheme=HTTPBearer()
 class AiPodcastClipper:
     @modal.enter()
     def load_model(self):
-        print("Loading model")
-        pass
+        print("Loading models")
+
+        self.whisperx_model = whisperx.load_model(
+            "large-v2", device="cuda", compute_type="float16")
+
+        self.alignment_model, self.metadata = whisperx.load_align_model(
+            language_code="en",
+            device="cuda"
+        )
+
+        print("Transcription models loaded...")
+
+
+        print("Creating gemini client...")
+        self.gemini_client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+        print("Created gemini client...")
+
+
+
+    def transcribe_video(self, base_dir: str, video_path: str) -> str:
+        audio_path = base_dir / "audio.wav"
+        extract_cmd = f"ffmpeg -i {video_path} -vn -acodec pcm_s16le -ar 16000 -ac 1 {audio_path}"
+        subprocess.run(extract_cmd, shell=True,
+                       check=True, capture_output=True)
+
+        print("Starting transcription with WhisperX...")
+        start_time = time.time()
+
+        audio = whisperx.load_audio(str(audio_path))
+        result = self.whisperx_model.transcribe(audio, batch_size=16)
+
+        result = whisperx.align(
+            result["segments"],
+            self.alignment_model,
+            self.metadata,
+            audio,
+            device="cuda",
+            return_char_alignments=False
+        )
+
+        duration = time.time() - start_time
+        print("Transcription and alignment took " + str(duration) + " seconds")
+
+        segments = []
+
+        if "word_segments" in result:
+            for word_segment in result["word_segments"]:
+                segments.append({
+                    "start": word_segment["start"],
+                    "end": word_segment["end"],
+                    "word": word_segment["word"],
+                })
+
+        return json.dumps(segments)
+    
+    def identify_moments(self, transcript: dict):
+        response = self.gemini_client.models.generate_content(model="gemini-2.5-flash-preview-04-17", contents="""
+    This is a podcast video transcript consisting of word, along with each words's start and end time. I am looking to create clips between a minimum of 30 and maximum of 60 seconds long. The clip should never exceed 60 seconds.
+
+    Your task is to find and extract stories, or question and their corresponding answers from the transcript.
+    Each clip should begin with the question and conclude with the answer.
+    It is acceptable for the clip to include a few additional sentences before a question if it aids in contextualizing the question.
+
+    Please adhere to the following rules:
+    - Ensure that clips do not overlap with one another.
+    - Start and end timestamps of the clips should align perfectly with the sentence boundaries in the transcript.
+    - Only use the start and end timestamps provided in the input. modifying timestamps is not allowed.
+    - Format the output as a list of JSON objects, each representing a clip with 'start' and 'end' timestamps: [{"start": seconds, "end": seconds}, ...clip2, clip3]. The output should always be readable by the python json.loads function.
+    - Aim to generate longer clips between 40-60 seconds, and ensure to include as much content from the context as viable.
+
+    Avoid including:
+    - Moments of greeting, thanking, or saying goodbye.
+    - Non-question and answer interactions.
+
+    If there are no valid clips to extract, the output should be an empty list [], in JSON format. Also readable by json.loads() in Python.
+
+    The transcript is as follows:\n\n""" + str(transcript))
+        print(f"Identified moments response: ${response.text}")
+        return response.text
 
     @modal.fastapi_endpoint(method="POST")
     def process_video(self,request:ProcessVideoRequest,token:HTTPAuthorizationCredentials=Depends(auth_scheme)):
-        print("Processing video"+request.s3_key)
-        pass
+        s3_key=request.s3_key
+
+        if(token.credentials != os.environ["AUTH_TOKEN"]):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Bearer Token",headers={"WWW-Authenticate": "Bearer"})
+        
+        run_id=str(uuid.uuid4())
+        base_dir = pathlib.Path("/tmp") / run_id
+        base_dir.mkdir(parents=True, exist_ok=True)
+
+        # Download video file
+        video_path = base_dir / "input.mp4"
+        s3_client = boto3.client("s3")
+        s3_client.download_file("clipify-ai", s3_key, str(video_path))
+
+        #1. Transcription
+        transcript_segments_json=self.transcribe_video(base_dir,video_path)
+        transcript_segments = json.loads(transcript_segments_json)
+
+        # 2. Identify moments for clips
+        print("Identifying clip moments")
+        identified_moments_raw = self.identify_moments(transcript_segments)
+
+        cleaned_json_string = identified_moments_raw.strip()
+        if cleaned_json_string.startswith("```json"):
+            cleaned_json_string = cleaned_json_string[len("```json"):].strip()
+        if cleaned_json_string.endswith("```"):
+            cleaned_json_string = cleaned_json_string[:-len("```")].strip()
+
+        clip_moments = json.loads(cleaned_json_string)
+        if not clip_moments or not isinstance(clip_moments, list):
+            print("Error: Identified moments is not a list")
+            clip_moments = []
+
+        print(clip_moments)
 
 @app.local_entrypoint()
 def main():
